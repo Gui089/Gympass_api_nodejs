56a9b44772796eec499b3e39b2531654176dac19
Gui089 <guilhermesilba811@gmail.com>
2025-01-18 14:30:42 -0300

    feat: add middleware jwt verify

--- src/http/controllers/authenticate-controller.ts
+++ src/http/controllers/authenticate-controller.ts
@@ -3,27 +3,38 @@ import { FastifyRequest, FastifyReply } from "fastify";
 import { InvalidCredentialsError } from "../../use-cases/errors/invalid-credential-error";
 import { makeAuthneticateUseCase } from "../../use-cases/factories/make-authneticate-use-case";
 
-export async function authenticateController(request:FastifyRequest, reply:FastifyReply){
+export async function authenticateController(request: FastifyRequest, reply: FastifyReply) {
 
     const authenticateBodySchema = z.object({
-        email:z.string().email(),
-        password:z.string().min(6)
+        email: z.string().email(),
+        password: z.string().min(6)
     });
-    
-    const {email, password} = authenticateBodySchema.parse(request.body);
+
+    const { email, password } = authenticateBodySchema.parse(request.body);
 
     try {
 
         const authenticateUseCase = makeAuthneticateUseCase();
 
-        await authenticateUseCase.execute({email,password});
-    } catch(err) {
-        if(err instanceof InvalidCredentialsError) {
-            return reply.status(409).send({message:err.message});
+        const { user } = await authenticateUseCase.execute({
+            email, password
+        });
+
+        const token = await reply.jwtSign(
+            {},
+            {
+                sign: {
+                    sub: user.id
+                }
+            });
+
+        return reply.status(200).send({ token });
+
+    } catch (err) {
+        if (err instanceof InvalidCredentialsError) {
+            return reply.status(409).send({ message: err.message });
         }
 
         throw err;
     }
-    
-    return reply.status(200).send();
 }
