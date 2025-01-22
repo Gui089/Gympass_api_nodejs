2f648afde3c1a6558b73c55207850c94778c03a8
meduardalima <guilherme@gui>
2025-01-22 20:23:32 -0300

    feat: add teste:e2e on the Npm scripts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,9 @@ import {defineConfig} from 'vitest/config';
 
 export default defineConfig({
     test: {
-      include: ['src/**/*.spec.ts'],
+      environment:"node",
+      environmentMatchGlobs:[['src/http/controllers/**', 'prisma']],
+      dir:'src'
     },
   });
   
